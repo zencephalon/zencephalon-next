f32ff08ec396f14cf2d556f9b9b92912d8bfd224
Matthew Bunday <mkbunday@gmail.com>
2021-10-05 10:57:50 -0400

    Move from layout to document

--- c/Layout.tsx
+++ c/Layout.tsx
@@ -8,13 +8,6 @@ import styles from "~/styles/Home.module.css";
 const Layout: React.FC = ({ children }) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/zenchinese.png" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Vollkorn&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
       <Topnav />
 
       {children}
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/zenchinese.png" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Vollkorn&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
