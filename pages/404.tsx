e5874044f6b7bde6e8285201e898493f766522a7
Matthew Bunday <mkbunday@gmail.com>
2022-05-31 16:21:04 -0400

    Add 404 and 500 pages

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+import Layout from "~/c/Layout";
+import Article from "~/c/Article";
+
+const NodePage: NextPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Page not Found</title>
+        <meta name="description" content={`404 on zencephalon.com`} />
+      </Head>
+
+      <main className={styles.main}>
+        <Article markdown="# Sorry\nCouldn't find this page." />
+      </main>
+    </Layout>
+  );
+};
+
+export default NodePage;
