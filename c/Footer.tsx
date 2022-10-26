2438e822e4510bea11879f273b56f78b22496301
Matthew Bunday <mkbunday@gmail.com>
2022-10-26 00:58:41 -0400

    Remove boilerplate

--- c/Footer.tsx
+++ c/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-const Footer: React.FC = ({ children }) => {
+const Footer = () => {
   return (
     <footer className="footer">
       <p>
